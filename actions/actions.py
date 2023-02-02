2f27c0e60f4612b69f16f84f7c046238736e2348
Faycel <faycel.sassi@esen.tn>
2023-02-02 16:48:59 +0100

    deleting unecessary intents and adding new action

--- actions/actions.py
+++ actions/actions.py
@@ -4,6 +4,7 @@ from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
+
 #the show time action
 class ActionHelloWorld(Action):
 
@@ -16,4 +17,27 @@ class ActionHelloWorld(Action):
 
         dispatcher.utter_message(text=f"{dt.datetime.now()}")
 
+        return []
+
+ 
+ #sending definitionf
+class ActionDefinion(Action):
+   
+    def name(self) -> Text:
+        return "action_give_def"
+  
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+ 
+        # play rock paper scissors
+        user_choice = tracker.get_slot("definition")
+        dispatcher.utter_message(text=f"You chose {user_choice}")
+
+ 
+        if user_choice == "tcp" :
+            dispatcher.utter_message(text="Transmission Control Protocol (TCP)  is one of the main protocols of the Internet protocol suite. It originated in the initial network implementation in which it complemented the Internet Protocol. Therefore, the entire suite is commonly referred to as TCP/IP.")
+        if user_choice == "nli" :
+            dispatcher.utter_message(text="Nli definition")
+        
         return []
