5d5e7b78af1240d2d9b2de842ab9faee864688fa
FaycelSassi <faycel.sassi@esen.tn>
2023-05-10 20:08:01 +0100

    Changing autocorrect and updating problem resoluti

--- actions/actions.py
+++ actions/actions.py
@@ -5,7 +5,11 @@ import re
 from io import BytesIO
 import base64
 import numpy as np
+import difflib
+import numpy as np
 from scipy import stats
+from pymongo import MongoClient, UpdateOne
+import pandas as pd
 import json
 from datetime import datetime, timedelta
 import pandas as pd
@@ -52,35 +56,56 @@ def get_simularity(w1, w2):
     return len(common)/max(len(bigram1), len(bigram2), 1)
 
 
-def autocorrect(word, coll, search):
+from scipy.spatial.distance import euclidean
+from sklearn.feature_extraction.text import CountVectorizer
 
-    # This function takes a word and a list of valid words, and returns the closest match to the input word from the list of valid words.
-    threshold = 0.6
+
+def autocorrect(input_word, coll,search,k=1):
     if search == 1:
         fields = ['abbrv', 'fullname']
     if search == 2:
         fields = ['BSC', 'Bande de fréquences', 'Gouvernorat', 'Site', 'Site_Code',
                   "Type d'Installation", 'Longitude', 'Latitude', 'LAC', 'Identifiant']
-    if search==3:
+    if search == 3:
         fields = ['ERBS Id']
-    distances = []
-    dbs = set()
     cursor = coll.find({})
+    dbs = []
     for document in cursor:
         for field in fields:
             if field in document:
-                word_list = set(document[field].split())
-                dbs.update(word_list)
-     # Get the list of similar words with their similarity score
-    similar_words = [(w, get_simularity(word, w)) for w in dbs]
+                word_list = str(document[field]).upper()
+                dbs.append(word_list)
+    return difflib.get_close_matches(input_word, dbs, n=k)
 
-    # Find the word with the highest similarity score above the threshold
-    best_word = max(
-        similar_words, key=lambda x: x[1] if x[1] >= threshold else -1)
-    print(best_word)
-    if best_word[1] < threshold:
-        return "none"
-    return best_word[0]
+# def autocorrect(word, coll, search):
+
+#     # This function takes a word and a list of valid words, and returns the closest match to the input word from the list of valid words.
+#     threshold = 0.6
+#     if search == 1:
+#         fields = ['abbrv', 'fullname']
+#     if search == 2:
+#         fields = ['BSC', 'Bande de fréquences', 'Gouvernorat', 'Site', 'Site_Code',
+#                   "Type d'Installation", 'Longitude', 'Latitude', 'LAC', 'Identifiant']
+#     if search==3:
+#         fields = ['ERBS Id']
+#     distances = []
+#     dbs = set()
+#     cursor = coll.find({})
+#     for document in cursor:
+#         for field in fields:
+#             if field in document:
+#                 word_list = set(document[field].split())
+#                 dbs.update(word_list)
+#      # Get the list of similar words with their similarity score
+#     similar_words = [(w, get_simularity(word, w)) for w in dbs]
+
+#     # Find the word with the highest similarity score above the threshold
+#     best_word = max(
+#         similar_words, key=lambda x: x[1] if x[1] >= threshold else -1)
+#     print(best_word)
+#     if best_word[1] < threshold:
+#         return "none"
+#     return best_word[0]
 
  # sending definition
 
@@ -205,7 +230,6 @@ class ActionSiteInfo(Action):
 
 
 def findsolution(dispatcher,x):
-
     client = pymongo.MongoClient("mongodb://localhost:27017/")
     db = client["rasa"]
     collection = db["Chap2"]
@@ -213,44 +237,44 @@ def findsolution(dispatcher,x):
     # create a Pandas DataFrame from the list of dictionaries
     Xdf = pd.DataFrame(data)
     if Xdf.empty:
-        dispatcher.utter_message("please add data to the database")
+        print("please add data to the database")
         return {}
     Xdf = Xdf.drop('_id', axis=1)
     # print(Xdf)
     paramlist = Xdf.values.tolist()
     solutionslist = []
     next = []
-    collection = db['Conteurs']
-    # retrieve documents with specified columns
+    collection = db['Compteurs']
+        # retrieve documents with specified columns
     documents = collection.find({})
     df = pd.DataFrame(list(documents))
     # print(df)
     k = 0
     j = 0
+    # param[0]== category 
+    # param[1]== next
+    # param[2]== solution
+    # param[3]==formule de calcul de résultat des conteurs
+    # param[4]==liste des conteurs à utiliser
     for param in paramlist:
         param[1] = param[1].replace(' ', '')
         param[0] = param[0].replace(' ', '')
-        param[2] = param[2].replace(' ', '')
-        if param[2] == 'notready' and param[4] == 'none':
+        # param[2] = param[2].replace(' ', '')
+        if param[2] == 'not ready' and param[4] == 'none':
             next = param[1].split(",")
         if param[0] in next:
-            if param[4] == 'none' and param[2] != 'notready':
+            if param[4] == 'none' and param[2] != 'not ready':
                 solutionslist.append(param[4])
-            if param[2] == 'notready' and param[4] != 'none':
+            if param[2] == 'not ready' and param[4] != 'none':
                 for x in param[4]:
-                    if x.replace(" ", "") not in df.columns:
-                        dispatcher.utter_message(
-                            x + " values not added in database")
-                    else:
+                    if x.replace(" ", "")  in df.columns:
                         k = 1
                 if k == 1:
                     i = param[4]
-                    if len(i) == 2:
+                    i = [s.replace(' ', '') for s in i]
+                    if (i[0]in df.columns) :
                         j += 1
-                        i[0].replace(" ", "")
-                        i[1].replace(" ", "")
-                        result = df.apply(lambda row: eval(param[3], {i[0].replace(" ", ""): row[i[0].replace(
-                            " ", "")], i[1].replace(" ", ""): row[i[1].replace(" ", "")]}), axis=1)
+                        result = df.eval(param[3])
                         df["resultat "+param[0]] = result
     s = []
     for j in range(len(df)):
@@ -262,7 +286,7 @@ def findsolution(dispatcher,x):
     next = ""
     for i in range(len(df)):
         for col in df.columns:
-            if df[col][i] == False:
+            if df[col][i] == True:
                 if "resultat" in col:
                     solist = []
                     x = col.replace("resultat ", "")
@@ -285,15 +309,27 @@ def findsolution(dispatcher,x):
                     next = ""
     df['solution'] = s
     # select columns A and C by name using loc, and convert to a list
-    selected_columns = df.loc[:, ['OSS_ID', 'SN',
-                                  'ELEMENT', 'MOID', 'solution']].values.tolist()
+    selected_columns = df.loc[:, ['ERBS Id', 'solution']].values.tolist()
+    i=0
+    s=[]
     for col in selected_columns:
-        if col[4] != '':
-            print(col[1])
-            dispatcher.utter_message(
-                    col[0]+" - "+col[1]+" : \n"+col[4]+" - ")
-
-
+        if col[1] != '':
+            i+=1
+            s.append(col[0])
+    my_string = ' , '.join(s) 
+    dispatcher.utter_message("there are problems in "+str(i)+" sites : "+my_string)
+    j=0
+    s=df['solution']
+    id=df['ERBS Id']
+    for i in id:
+        collection.update_many(
+                {'ERBS Id':i},
+                {"$set":
+                    {
+                        "Solution": s[j]
+                    }
+                })
+        j+=1
 
 
 # the reset all slots action
@@ -305,14 +341,71 @@ class ActionProblemSolve(Action):
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
 
-        # file_path = os.path.join(os.path.dirname(__file__), 'LteParams.xlsx')
-        # params = pd.read_excel(file_path)
-        # retrieve all documents from the collection
         findsolution(dispatcher,0)
         return {}
 
 # the reset all slots action
 
+class ActionInfoProblem(Action):
+    def name(self):
+        return "action_Info_Problem"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        collection = db['Compteurs']
+        # retrieve documents with specified columns
+        documents = collection.find({})
+        df = pd.DataFrame(list(documents))
+        selected_columns = df.loc[:, ['ERBS Id', 'solution']].values.tolist()
+        for col in selected_columns:
+            if col[1] != '':
+                dispatcher.utter_message(col[0]+':\n'+col[1])
+        return{}
+
+class ActionSiteProblem(Action):
+    def name(self):
+        return "action_Site_Problem"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+        client = pymongo.MongoClient("mongodb://localhost:27017/")
+        db = client["rasa"]
+        collection = db["Traffic"]
+        msg=tracker.latest_message.get("text")
+        delimiters = ["for ", "of "]
+        regex_pattern = '|'.join(map(re.escape, delimiters))
+        split_string = re.split(regex_pattern, msg)[1]
+        split_string=split_string.replace(" ","_")
+        if '4G' not in split_string:
+            split_string='4G_'+split_string
+        print(split_string)
+        split_string=split_string.upper()
+        print(split_string)
+        site=autocorrect(split_string,collection,3)
+        print(site)
+        if len(site)!=0:
+            site=site[0]
+        collection = db['Compteurs']
+        if site!="none":
+            # retrieve documents with specified columns
+            documents = collection.find({})
+            df = pd.DataFrame(list(documents))
+            if 'Solution' not in df.columns: 
+                findsolution(dispatcher,0)
+            selected_columns = df.loc[:, ['ERBS Id', 'Solution']].values.tolist()
+            for col in selected_columns:
+                if col[0].upper() == site:
+                    if col[1]=="":
+                        dispatcher.utter_message("there's no problem with "+col[0])
+                    else:   
+                        dispatcher.utter_message('the solution for '+col[0]+' is : '+ col[1])
+
+                    return [SlotSet("site", col[0])]
+        else:  
+            dispatcher.utter_message('please verify input')
+            return [SlotSet("site", site)] 
 
 class ActionProblemSolveML(Action):
     def name(self):
@@ -538,56 +631,59 @@ class ActionPredictTraffic(Action):
         model.add(Flatten())
         model.compile(optimizer='Nadam', loss='mean_squared_error')
         print(model.summary())
-        model.fit(trainX, trainY, validation_data=(testX, testY),
-          verbose=2, epochs=50)
+        r2=0.5
+        while(r2<0.65):
+            model.fit(trainX, trainY, validation_data=(testX, testY),
+            verbose=2, epochs=50)
 
-        # make predictions
+            # make predictions
 
-        trainPredict = model.predict(trainX)
-        testPredict = model.predict(testX)
-        # invert predictions back to prescaled values
-        #This is to compare with original input values
-        #SInce we used minmaxscaler we can now use scaler.inverse_transform
-        #to invert the transformation.
-        trainPredict = scaler.inverse_transform(trainPredict)
-        trainY = scaler.inverse_transform([trainY])
-        testPredict = scaler.inverse_transform(testPredict)
-        testY = scaler.inverse_transform([testY])
+            trainPredict = model.predict(trainX)
+            testPredict = model.predict(testX)
+            # invert predictions back to prescaled values
+            #This is to compare with original input values
+            #SInce we used minmaxscaler we can now use scaler.inverse_transform
+            #to invert the transformation.
+            trainPredict = scaler.inverse_transform(trainPredict)
+            trainY = scaler.inverse_transform([trainY])
+            testPredict = scaler.inverse_transform(testPredict)
+            testY = scaler.inverse_transform([testY])
 
-        # calculate root mean squared error
-        trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
-        print('Train Score: %.2f RMSE' % (trainScore))
-        dispatcher.utter_message('Train Score par RMSE: '+str(trainScore))
-        testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
-        print('Test Score: %.2f RMSE' % (testScore))
-        dispatcher.utter_message('Test Score par RMSE: ' + str(testScore))
-        r2 = r2_score(testY[0], testPredict[:,0])
-        print('R2 score:', r2)
-        dispatcher.utter_message('R2 score: '+ str(r2))
-        # shift train predictions for plotting
-        #we must shift the predictions so that they align on the x-axis with the original dataset. 
-        trainPredictPlot = np.empty_like(dataset)
-        trainPredictPlot[:, :] = np.nan
-        trainPredictPlot[seq_size:len(trainPredict)+seq_size, :] = trainPredict
+            # calculate root mean squared error
+            trainScore = math.sqrt(mean_squared_error(trainY[0], trainPredict[:,0]))
+            print('Train Score: %.2f RMSE' % (trainScore))
+            dispatcher.utter_message('Train Score par RMSE: '+str(trainScore))
+            testScore = math.sqrt(mean_squared_error(testY[0], testPredict[:,0]))
+            print('Test Score: %.2f RMSE' % (testScore))
+            dispatcher.utter_message('Test Score par RMSE: ' + str(testScore))
+            r2 = r2_score(testY[0], testPredict[:,0])
+            print('R2 score:', r2)
+            dispatcher.utter_message('R2 score: '+ str(r2))
+            # shift train predictions for plotting
+            #we must shift the predictions so that they align on the x-axis with the original dataset. 
+            trainPredictPlot = np.empty_like(dataset)
+            trainPredictPlot[:, :] = np.nan
+            trainPredictPlot[seq_size:len(trainPredict)+seq_size, :] = trainPredict
 
-        # shift test predictions for plotting
-        testPredictPlot = np.empty_like(dataset)
-        testPredictPlot[:, :] = np.nan
-        testPredictPlot[len(trainPredict)+(seq_size*2)+1:len(dataset)-1, :] = testPredict
+            # shift test predictions for plotting
+            testPredictPlot = np.empty_like(dataset)
+            testPredictPlot[:, :] = np.nan
+            testPredictPlot[len(trainPredict)+(seq_size*2)+1:len(dataset)-1, :] = testPredict
 
-        # plot baseline and predictions
-        plt.figure(figsize=(20,10))
-        plt.plot(scaler.inverse_transform(dataset))
-        plt.plot(trainPredictPlot)
-        plt.plot(testPredictPlot)
-        plt.legend(['Data', 'Train Predict result', 'Test Predict result'])
-        # Save the plot to a temporary buffer
-        buf = BytesIO()
-        plt.savefig(buf, format='png')
-        buf.seek(0)
+            # plot baseline and predictions
+            plt.figure(figsize=(20,10))
+            plt.plot(scaler.inverse_transform(dataset))
+            plt.plot(trainPredictPlot)
+            plt.plot(testPredictPlot)
+            plt.legend(['Data', 'Train Predict result', 'Test Predict result'])
+            # Save the plot to a temporary buffer
+            buf = BytesIO()
+            plt.savefig(buf, format='png')
+            buf.seek(0)
 
-        # Encode the image as base64
-        image = base64.b64encode(buf.read()).decode('utf-8')
+            # Encode the image as base64
+            image = base64.b64encode(buf.read()).decode('utf-8')
 
-        # Send the image using the dispatcher
-        dispatcher.utter_message(image=f"data:image/png;base64,{image}")
+            # Send the image using the dispatcher
+            dispatcher.utter_message(image=f"data:image/png;base64,{image}")
+        return [SlotSet("site", site)]
